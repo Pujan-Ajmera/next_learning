cbb098f6d56dc12f5f4f9916ca2e72c0e5746f3d
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2025-11-24 20:08:21 +0530

    Updated practical files

---
+++ app/home/inner_home/page.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+import { count } from 'console';
+import React, { useState } from 'react';
+export default function Home() {
+  return (
+    <>
+        <h1>Inner Home Page</h1>
+    </>
+  );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+import { count } from 'console';
+import React, { useState } from 'react';
+export default function Home() {
+  return (
+    <>
+        <h1>Home Page</h1>
+    </>
+  );
+}
